866417023f4ae3eda308ef4c709dbc34c81c93f4
Sesiliahongoli <135832955+Sesiliahongoli@users.noreply.github.com>
2023-06-16 08:49:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ st.title("Guess the Number Game")
 number = random .randint(1,100)
 
 guess  = st. number_input("Enter a number (between 1 and 100):",min_value=1,max_value=100)
+
 if st.button("Make a guess!"):
   if guess > number :
-    st.writer("Too high! Try a smaller number.")
-elif guess < number:
+    st.write("Too high! Try a smaller number.")
+  elif guess < number:
      st.write("Too low! Try a larger number.")
-else:
+  else:
      st.write("Congratulations ! You have guessed the number correctly.")
