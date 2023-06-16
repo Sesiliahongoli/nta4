b054254122e6281480c016da21f738480b4da546
Sesiliahongoli <135832955+Sesiliahongoli@users.noreply.github.com>
2023-06-16 09:30:55 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import random
+
+st.title('Guess the Number Game')
+
+#generate arandom number between 1 and 100
+number = random.randint(1, 100)
+
+guess  = st. number_input("Enter a number (between 1 and 100):", min_value=1,max_value=100)
+
+if st.button('Make a guess!'):
+    if guess > number :
+        st.write('Too high! Try a smaller number.')
+    elif guess < number:
+         st.write('Too low! Try a larger number.')
+    else:
+        st.write('Congratulations ! You have guessed the number correctly.')
